e053607dccd0d0ab7ade27e4a8651467d4e32633
Yokhalakshmi-K <162993673+Yokhalakshmi-K@users.noreply.github.com>
2024-03-14 11:56:51 +0530

    Update WaysToReach.java

--- advanced/WaysToReach.java
+++ advanced/WaysToReach.java
@@ -1,3 +1,7 @@
+/*There is an N by M matrix of zeroes. Given N and M, write a function to count the
+number of ways of starting at the top-left corner and getting to the bottom-right corner.
+You can only move right or down.*/
+
 import java.util.*;
 public class WaysToReach{
 	public static void main(String[] args){
